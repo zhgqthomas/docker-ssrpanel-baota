81d0afa4d755b33933b87e168da5101ad4b1d28c
zhgqthomas <zhgqthomas@gmail.com>
2018-01-10 11:13:27 +0800

    Add readme & comment in  start.sh

---
+++ README
@@ -0,0 +1,3 @@
+### Build [SSRPanel](https://github.com/ssrpanel/SSRPanel) with [Baota](http://www.bt.cn/)
+
+Use Docker for building SSRPanel website driven by Baota
--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# automatically start a service when running a docker container
+# checkout https://stackoverflow.com/questions/25135897/how-to-automatically-start-a-service-when-running-a-docker-container
+
 echo 'starting bt....'
 service bt start
 
